611be4041e851f0709c8b75c0eb79c5c02a8f8d9
Dementr <dementr@mail.com>
2023-02-08 22:31:21 +0300

    Регистрация/Авторизация  -  Вынес в отдельный файл поля классов Forms.py / добавил логику авторизации и регистрации (AAA)

---
+++ users/forms.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from users.models import User
+from django import forms
+
+class UserLogForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+
+
+    class Meta:                          #он принимает доп параметры котор отвечают за то с какой моделью будет работать форма и с какими полями
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+
--- users/views.py
+++ users/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import User
+from users.forms import UserLogForm, UserRegForm
+from django.contrib import auth
+from django.urls import reverse
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method =='POST':                         #КОНТРОЛЬ // проверка на пост запрос
+        form = UserLogForm(data=request.POST)           # вызываем класс , передавая параметр - данные которые были заполнены в форме
+        if form.is_valid():                             # проверка на валидность
+            username = request.POST['username']         # достаем данные username
+            password = request.POST['password']         # достаем данные password
+            user = auth.authenticate(username=username,password=password)       # ПОДТВЕРЖДЕНИЕ ПОДЛИННОСТИ // проверка есть ли пользователь
+            if user:                                    # если он(пользователь) есть
+                auth.login(request, user)               # мы его авторизовали  // РАЗРЕШЕНИЕ
+                return HttpResponseRedirect('/')     # немного не правильно надо бы на url исправить // пернаправляем на главную страницу
+    else:
+        form = UserLogForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
